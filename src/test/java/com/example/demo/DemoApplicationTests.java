d67207455bd421f18b3e63bc90021730cb26f215
Lukasz Klonowski <lukasz.klonowski.sp@lhsystems.com>
2022-05-11 09:51:51 +0200

    Add test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.io.File;
+
 class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	void checkFilePath() {
+        final File file = new File("src/main/resources/file.txt");
+        System.out.println(file.getAbsolutePath());
+        Assertions.assertTrue(file.getAbsolutePath().contains("demo"));
+    }
 
 }
